25ec60d8adeb46f35d016a06e76ff60784efc624
Mitchell J Stanton-Cook <m.stantoncook@gmail.com>
2014-09-04 13:53:22 +1000

    Versioning

---
+++ src/GenomeBaser/__init__.py
@@ -0,0 +1,7 @@
+__title__         = 'GenomeBaser'
+__version__       = '0.1.0'
+__description__   = "GenomeBaser manages complete (bacterial) genomes from NCBI"
+__author__        = 'Mitchell Stanton-Cook'
+__author_email__  = 'm.stantoncook@gmail.com'
+__url__           = 'http://github.com/mscook/GenomeBaser'
+__license__       = 'ECL 2.0'
